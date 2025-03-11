98399ef0055b043a120fa3a7128a404a84679904
BUser <mymail@gmx.de>
2025-03-11 11:11:07 +0100

    Person zugefügt

---
+++ src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    private String firstName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
